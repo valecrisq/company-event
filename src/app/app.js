6cc54714c16e1225ffc3e1872017a58401602ebf
Flavio Tulino <>
2018-08-27 15:26:37 +0100

    first commit

--- src/app/app.js
+++ src/app/app.js
@@ -1,32 +1,11 @@
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
 
-import home from './components/home/home';
-import about from './components/about/about';
-
-import '../style/app.css';
 import template from './app.html';
+import '../style/app.css';
 
-export class AppCtrl {
-  constructor($log) {
-    // $log.info('AppCtrl instantiated');
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
+export default angular
+    .module('app', [])
+    .component('app', {
+        template
+    }).name;
 
-AppCtrl.$inject = ['$log'];
-
-export const appDirective = {
-    template: template,
-    controller: AppCtrl,
-};
-
-const MODULE_NAME = 'app';
-
-angular.module(MODULE_NAME, [
-  home,
-  about,
-])
-  .component('app', appDirective);
-
-export default MODULE_NAME;
